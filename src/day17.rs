b03a46a37b90908700654e680d4ec2f7a4f4c03b
newcat <freddy.wagner@web.de>
2021-12-27 22:33:42 +0100

    Cleanup day 17

--- src/day17.rs
+++ src/day17.rs
@@ -84,73 +84,8 @@ fn fuzz(target: &TargetArea, min_x_guess: i32) -> (SimulationResult, usize) {
   return (best_simulation.unwrap(), valid_simulations);
 }
 
-fn get_pos_for_step(vx_0: i32, vy_0: i32, step: i32) -> [i32; 2] {
-  let mut vx = vx_0;
-  let mut vy = vy_0;
-  let mut x = 0;
-  let mut y = 0;
-  for _ in 1..=step {
-    x += vx;
-    y += vy;
-    if vx > 0 {
-      vx -= 1;
-    }
-    vy -= 1;
-  }
-  return [x, y];
-}
-
-fn get_min_steps_for_x(target: &TargetArea) -> i32 {
-  let mut s = 1;
-  loop {
-    let x = s * s - (((s - 1) * (s - 1) + s - 1) / 2);
-    if x > target.xmin {
-      return s;
-    }
-    s += 1;
-  }
-}
-
-/**
- * (s, vy_0)
- */
-fn get_steps_for_max_y(target: &TargetArea, vx_0: i32) -> (i32, i32) {
-  let mut y_guess = 1;
-  loop {
-    let mut s = vx_0;
-    let mut prev_y = 0;
-    'inner: loop {
-      let x_res = s * vx_0 - (((s - 1) * (s - 1) + s - 1) / 2);
-      let y_res = s * y_guess - (((s - 1) * (s - 1) + s - 1) / 2);
-      if prev_y > target.ymax && y_res < target.ymin {
-        return (s, y_guess - 1);
-      } else if x_res >= target.xmin
-        && x_res <= target.xmax
-        && y_res >= target.ymin
-        && y_res <= target.ymax
-      {
-        break 'inner;
-      } else if y_res < target.ymin {
-        break 'inner;
-      }
-      prev_y = y_res;
-      s += 1;
-    }
-    y_guess += 1;
-  }
-}
-
 fn part1(lines: &readfile::Lines) {
   let target = TargetArea::new(lines.lines().next().unwrap());
-  // let target = TargetArea::new("target area: x=20..30, y=-10..-5");
-
-  let vx_0 = get_min_steps_for_x(&target);
-  // let (steps, vy_0) = get_steps_for_max_y(&target, vx_0);
-
-  /*let max_y = (1..=steps)
-  .map(|s| s * vy_0 - (((s - 1) * (s - 1) + s - 1) / 2))
-  .max()
-  .unwrap();*/
 
   let fuzz_result = fuzz(&target, 0);
   let SimulationResult {
